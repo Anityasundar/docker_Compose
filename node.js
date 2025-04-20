92c9ed00622e65a96b56d1340f508a096d43aa56
Anityasundar <anityasundar53@gmail.com>
2025-04-21 02:52:11 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,38 +1,9 @@
 const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('frontend'));
+const port = 3000;
 
-const db = mysql.createConnection({
-  host: 'db',
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Website running at http://localhost:${port}`);
 });
-
-db.connect(err => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
-});
-
-app.get('/api/guest', (req, res) => {
-  db.query('SELECT * FROM guests', (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-});
-
-app.post('/api/guest', (req, res) => {
-  const { name, message } = req.body;
-  db.query('INSERT INTO guests (name, message) VALUES (?, ?)', [name, message], (err) => {
-    if (err) throw err;
-    res.sendStatus(200);
-  });
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
